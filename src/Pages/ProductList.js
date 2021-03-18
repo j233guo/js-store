65aec1a19631aead78e6d91970e9f053c9156f6a
Jiaming Guo <gjm.mailbox2@gmail.com>
2021-03-18 16:58:15 -0400

    supported pagination

--- src/Pages/ProductList.js
+++ src/Pages/ProductList.js
@@ -1,51 +1,18 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
+import Pagination from 'react-js-pagination'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 import ProductListItems from '../Components/ProductListItems'
 
 const ProductList = (props) => {
 
-    const [products, setProducts] = useState([]);
     const [title, setTitle] = useState([]);
-    const filterProduct = (productList) => {
-        if (props.match.params.p === "bestseller") {
-            let best = productList.filter((product) => {
-                return product.bestseller === true
-            });
-            setProducts(best)
-            setTitle("Best Sellers")
-        } else if (props.match.params.p === "Home") {
-            let home = productList.filter((product) => {
-                return product.category === "Home"
-            });
-            setProducts(home)
-            setTitle("Home")
-        } else if (props.match.params.p === "Computers") {
-            let computers = productList.filter((product) => {
-                return product.category === "Computers"
-            });
-            setProducts(computers)
-            setTitle("Computers")
-        } else if (props.match.params.p === "Electronics") {
-            let electronics = productList.filter((product) => {
-                return product.category === "Electronics"
-            });
-            setProducts(electronics)
-            setTitle("Electronics")
-        } else if (props.match.params.p === "Clothing") {
-            let clothing = productList.filter((product) => {
-                return product.category === "Clothing"
-            });
-            setProducts(clothing)
-            setTitle("Clothings")
-        } else if (props.match.params.p === "all") {
-            setProducts(productList)
-            setTitle("All Products")
-        }
-    }
+    const [rows, setRows] = useState([]);
+    const [activePage, setActivePage] = useState(1);
 
+    // spliting listings into chunks of 3
     const chunk = (arr, size) => {
         let cache = [];
         const tmp = [...arr];
@@ -58,18 +25,63 @@ const ProductList = (props) => {
         return cache
     }
 
-    const productsChunks = chunk(products, 3)
+    const filterProduct = (productList) => {
+        if (props.match.params.p === "bestseller") {
+            let best = productList.filter((product) => {
+                return product.bestseller === true
+            });
+            setTitle("Best Sellers")
+            setRows(chunk(best, 3))
+        } else if (props.match.params.p === "Home") {
+            let home = productList.filter((product) => {
+                return product.category === "Home"
+            });
+            setTitle("Home")
+            setRows(chunk(home, 3))
+        } else if (props.match.params.p === "Computers") {
+            let computers = productList.filter((product) => {
+                return product.category === "Computers"
+            });
+            setTitle("Computers")
+            setRows(chunk(computers, 3))
+        } else if (props.match.params.p === "Electronics") {
+            let electronics = productList.filter((product) => {
+                return product.category === "Electronics"
+            });
+            setTitle("Electronics")
+            setRows(chunk(electronics,3))
+        } else if (props.match.params.p === "Clothing") {
+            let clothing = productList.filter((product) => {
+                return product.category === "Clothing"
+            });
+            setTitle("Clothings")
+            setRows(chunk(clothing, 3))
+        } else if (props.match.params.p === "all") {
+            setTitle("All Products")
+            setRows(chunk(productList, 3))
+        }
+    }
 
-    const rows = productsChunks.map((productChunk, index) => {
-        const productsCols = productChunk.map((product) => {
+    // Pagination
+    const rowsPerPage = 4;
+    const indexOfLastRow  = activePage * rowsPerPage;
+    const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+    const currentRows = rows.slice( indexOfFirstRow, indexOfLastRow );
+
+    const renderRows = currentRows.map((row, index) => {
+        const productsCols = row.map((product) => {
             return (
                 <Col xs="4" key={product.id}>
                     <ProductListItems key={product.id} id={product.id} title={product.title} price={product.Price} img={product.img}/>	  
                 </Col>
             );
         });
-        return <Row key={index} style={{marginTop:"50px"}}>{productsCols}</Row>
+        return <Row key={index} style={{marginTop:"30px"}}>{productsCols}</Row>
     });
+
+    const handlePageChange = (pageNumber) => {
+        setActivePage( pageNumber )
+    };
 
     useEffect(() => {
             const url="https://js-store-db.herokuapp.com/products";
@@ -88,7 +100,17 @@ const ProductList = (props) => {
         <Header/>
             <Container style={{marginTop:"3rem", marginBottom:"10rem"}}>
                 <h1>{title}</h1>
-                {rows}
+                { renderRows }
+                <Pagination
+                    activePage={ activePage }
+                    itemsCountPerPage={ 4 }
+                    totalItemsCount={ rows.length }
+                    pageRangeDisplayed={ 3 }
+                    onChange={ handlePageChange }
+                    hideFirstLastPages="true"
+                    nextPageText=">"
+                    prevPageText="<"
+                />
             </Container>
         <Footer/>
         </>
--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 import {Container, Col, Row} from 'react-bootstrap'
+import Pagination from 'react-js-pagination'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 import ProductListItems from '../Components/ProductListItems'
@@ -7,24 +8,10 @@ import ProductListItems from '../Components/ProductListItems'
 const SearchPage = (props) => {
 
     const [products, setProducts] = useState([]);
+    const [rows, setRows] = useState([]);
+    const [activePage, setActivePage] = useState(1);
 
-    const filterProduct = (productList) => {
-
-        if (props.match.params.name !== "All") {
-            productList = productList.filter((product) => {
-                return product.name.includes(props.match.params.name)
-            })
-        }
-        if (props.match.params.category === "All") {
-            setProducts(productList);
-        } else {
-            productList = productList.filter((product) => {
-                return product.category === props.match.params.category
-            })
-            setProducts(productList)
-        }
-    }
-
+    // spliting listings into chunks of 3
     const chunk = (arr, size) => {
         let cache = [];
         const tmp = [...arr];
@@ -36,19 +23,45 @@ const SearchPage = (props) => {
         }
         return cache
     }
+    
+    const filterProduct = (productList) => {
+        if (props.match.params.name !== "All") {
+            productList = productList.filter((product) => {
+                return product.name.includes(props.match.params.name)
+            })
+        }
+        if (props.match.params.category === "All") {
+            setProducts(productList);
+            setRows(chunk(productList, 3))
+        } else {
+            productList = productList.filter((product) => {
+                return product.category === props.match.params.category
+            })
+            setProducts(productList)
+            setRows(chunk(productList, 3))
+        }
+    }
 
-    const productsChunks = chunk(products, 3)
+    // Pagination
+    const rowsPerPage = 4;
+    const indexOfLastRow  = activePage * rowsPerPage;
+    const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+    const currentRows = rows.slice( indexOfFirstRow, indexOfLastRow );
 
-    const rows = productsChunks.map((productChunk, index) => {
-        const productsCols = productChunk.map((product) => {
+    const renderRows = currentRows.map((row, index) => {
+        const productsCols = row.map((product) => {
             return (
                 <Col xs="4" key={product.id}>
                     <ProductListItems key={product.id} id={product.id} title={product.title} price={product.Price} img={product.img}/>	  
                 </Col>
             );
         });
-        return <Row key={index}>{productsCols}</Row>
+        return <Row key={index} style={{marginTop:"30px"}}>{productsCols}</Row>
     });
+
+    const handlePageChange = (pageNumber) => {
+        setActivePage( pageNumber )
+    };
 
     useEffect(() => {
         const url="https://js-store-db.herokuapp.com/products"
@@ -67,7 +80,17 @@ const SearchPage = (props) => {
         <Header/>
             <Container style={{marginTop:"3rem", marginBottom:"10rem"}}>
                 <h2>Search Results for "{props.match.params.name}" in {props.match.params.category}</h2>
-                {rows}
+                { renderRows }
+                <Pagination
+                    activePage={ activePage }
+                    itemsCountPerPage={ 4 }
+                    totalItemsCount={ rows.length }
+                    pageRangeDisplayed={ 3 }
+                    onChange={ handlePageChange }
+                    hideFirstLastPages="true"
+                    nextPageText=">"
+                    prevPageText="<"
+                />
                 <h3 className={products.length === 0? "" : "hide"}>Sorry, no results found....</h3>
             </Container>
         <Footer/>
